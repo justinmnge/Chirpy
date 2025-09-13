4ec0e9b0fe919cc9982338b7963d806d3cc54d43
justinmnge <justin.mnge@gmail.com>
2025-09-13 13:54:55 -0600

    feat ✨: Added a readiness endpoint to the Chirpy server

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/", fs)
+	fs := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", fs)
+	mux.HandleFunc("/healthz", healthz)
 	srv := http.Server{Addr: ":8080", Handler: mux}
 
 	if err := srv.ListenAndServe(); err != nil {
